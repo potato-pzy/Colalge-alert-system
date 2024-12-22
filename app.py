a3d99960ab7a52b19280459b9a2cdcd143fa0fb4
Ahzaaf.S <151323574+potato-pzy@users.noreply.github.com>
2024-12-22 20:31:03 +0530

    Update app.py port error app

--- app.py
+++ app.py
@@ -296,5 +296,6 @@ def init_admin(app):
             app.mongo_admins.insert_one(admin_data)
 
 if __name__ == '__main__':
+    app = create_app()
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
