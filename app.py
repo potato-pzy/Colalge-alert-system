c9f18350478b9a4435a26abe6a157d94d49badce
Ahzaaf.S <151323574+potato-pzy@users.noreply.github.com>
2024-12-22 20:25:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -283,6 +283,5 @@ def init_admin(app):
             app.mongo_admins.insert_one(admin_data)
 
 if __name__ == '__main__':
-    app = create_app()
-    init_admin(app)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
