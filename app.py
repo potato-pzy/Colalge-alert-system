ed1126311a80d8748f8026e9cc5e1d3ced46c40c
Ahzaaf.S <151323574+potato-pzy@users.noreply.github.com>
2025-01-11 11:37:15 +0530

    The update

--- app.py
+++ app.py
@@ -179,12 +179,6 @@ def create_app():
 
         return render_template('passwordreset.html')
 
-    # More routes...
-    
-    return app
-
-
-
     @app.route('/logout')
     def logout():
         session.clear()
